96bb312ed2daa8a4423faf80de7ed42331a20df6
Compu5 <compu5@gmail.com>
2026-05-28 15:07:12 -0600

    Aqui

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,35 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import { supabase } from './utils/supabase'
-import MyMapComponent from './components/myMapComponent';
-import './assets/styles/MapaHome.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ArchiveroPage from "./pages/ArchiveroPage";
-import Perfil from "./pages/perfil";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import './assets/styles/MapaHome.css'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import ArchiveroPage from "./pages/ArchiveroPage"
+import Perfil from "./pages/perfil"
+import Agente from "./components/AgenteCoraChat"
+
+
 
 
 function App() {
-  const [todos, setTodos] = useState([])
 
-  useEffect(() => {
-    async function getTodos() {
-      const { data: todos } = await supabase.from('todos').select()
-
-      if (todos) {
-        setTodos(todos)
-      }
-    }
-
-    getTodos()
-  }, [])
-
-
-  const currentPath = window.location.pathname.toLowerCase();
-
-  if (currentPath === '/archivero') {
-    return <ArchiveroPage />;
-  }
-  if (currentPath === '/perfil') {
-    return <Perfil />;
-  }
   return (
-    <div className="app">
-      <Header />
-      <div className="map-container">
-        <MyMapComponent />
-      </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/archivero" element={<ArchiveroPage />} />
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
       <Footer />
-    </div>
-  );
+      <Agente />
+      
+    </BrowserRouter>
+    
+  )
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import MyMapComponent from '../components/myMapComponent'
+
+
+function Home() {
+  return (
+    <div className="app">
+      <div className="map-container">
+        <MyMapComponent />
+      </div>
+    </div>
+  )
+}
+export default Home
